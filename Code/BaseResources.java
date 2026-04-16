8df6067ec9c0a3c588537589edacdd25863d0600
HuzaifahSajjad <hmsajjad@my.waketech.edu>
2026-04-16 10:19:07 -0400

    Create BaseResources.java

---
+++ Code/BaseResources.java
@@ -0,0 +1,7 @@
+public class BaseResources {
+    protected String name;
+    protected String description;
+    protected String type;
+    protected int consumptionPoints;
+    protected int sellMoneys;
+}
