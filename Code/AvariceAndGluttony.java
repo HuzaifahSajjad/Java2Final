a3204e87976e9249ced8c3ca5afb8f64563da8dd
HuzaifahSajjad <hmsajjad@my.waketech.edu>
2026-04-20 19:58:42 -0400

    Update AvariceAndGluttony.java

--- Code/AvariceAndGluttony.java
+++ Code/AvariceAndGluttony.java
@@ -1,5 +1,33 @@
 public class AvariceAndGluttony {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("The worst day of your life has occurred.");
+        Thread.sleep(4000);
+        System.out.println("You don't even want to think about the possibility... that this is real.");
+        Thread.sleep(4000);
+        System.out.println("That the wonderful, wonderful Phil Collins");
+        Thread.sleep(1000);
+        System.out.println("is no more.");
+        System.out.println();
+        Thread.sleep(3500);
+        System.out.println("There's no point to anything anymore.");
+        Thread.sleep(3500);
+        System.out.println("Your life means nothing.");
+        Thread.sleep(3500);
+        System.out.println("Nothing can fill the hole in your heart...");
+        Thread.sleep(2000);
+        System.out.println("You have ran off into the mountains to cry, but from the heights you notice something.");
+        Thread.sleep(2000);
+        System.out.println("A town... probably living a sad life, with Phil Collins dead and everything...");
+        Thread.sleep(2000);
+        System.out.println("You suddenly feel like attacking it and stealing stuff for some reason.");
+        Thread.sleep(2000);
+        System.out.println("Probably because you don't care anymore.");
+        Thread.sleep(2000);
+        System.out.println("After all, with Phil Collins dead...");
+        Thread.sleep(2000);
+        System.out.println("The only thing left for you to do... is satiate your...");
+        Thread.sleep(2000);
+        System.out.println("!! AVARICE & GLUTTONY !!\ntype start to play");
     }
 }
