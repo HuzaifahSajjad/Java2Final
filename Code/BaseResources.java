6868020bbf148604646612a7c55c486e62271229
HuzaifahSajjad <hmsajjad@my.waketech.edu>
2026-04-20 19:59:05 -0400

    Update BaseResources.java

--- Code/BaseResources.java
+++ Code/BaseResources.java
@@ -4,4 +4,20 @@ public class BaseResources {
     protected String type;
     protected int consumptionPoints;
     protected int sellMoneys;
+    public static String[] resourceTypes = {"Scavenged", "Crafted", "Spooned"};
+    public static String[] allResources = {
+            "Pollen",
+            "Rowdy Crude",
+            "Silicon",
+            "Fruit",
+            "Chill Pollens",
+            "Soul of Phil Collins",
+            "Fractured Soul of Phil Collins",
+            "Oil",
+            "Samosas",
+            "Gun",
+            "Spoonerism Machine",
+            "Impoliteness",
+            "Lighter"
+            };
 }
