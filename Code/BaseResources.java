0270da31058662ce1d4a1f958b0c08cf613969b6
HuzaifahSajjad <hmsajjad@my.waketech.edu>
2026-04-21 10:16:40 -0400

    Update BaseResources.java

--- Code/BaseResources.java
+++ Code/BaseResources.java
@@ -1,9 +1,12 @@
 public class BaseResources {
     protected String name;
-    protected String description;
+    protected String obtainDescription;
+    protected String analyzeDescription;
+    protected String useDescription;
+    protected String sellDescription;
     protected String type;
-    protected int consumptionPoints;
-    protected int sellMoneys;
+    protected int consumptionPoints = 0;
+    protected int sellMoneys = 0;
     public static String[] resourceTypes = {"Scavenged", "Crafted", "Spooned"};
     public static String[] allResources = {
             "Pollen",
@@ -18,6 +21,22 @@ public class BaseResources {
             "Gun",
             "Spoonerism Machine",
             "Impoliteness",
-            "Lighter"
-            };
+            "Lighter",
+            "Ice"
+    };
+    public Resources (String name) {
+        this.name = name;
+        switch(name) {
+            case "Pollen":
+                this.name = name;
+                this.obtainDescription = "You decided to enslave the local bee population. Swathes of pollen appear at your feet!";
+                this.analyzeDescription = "Your eyes and nose feel horrendous... but you heard that this stuff tastes real good.";
+                this.useDescription = "You sneezed a bunch, but you consumed the Pollen. Your Gluttony is slightly satiated (+3)";
+                this.sellDescription = "You sold the Pollen in a bucket. Your Avarice is slightly satiated (+$18)";
+                this.type = resourceTypes[0];
+                this.consumptionPoints = 3;
+                this.sellMoneys = 18;
+                break;
+        }
+    }
 }
